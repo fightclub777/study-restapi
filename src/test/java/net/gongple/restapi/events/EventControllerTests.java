a2980620a698842ff2f2969034d76629e9d5f948
선영 맹 <imdct@seon-yeong-maeng-ui-MacBook-Pro.local>
2019-10-24 20:22:10 +0900

    POST /api/events returns 201

--- src/test/java/net/gongple/restapi/events/EventControllerTests.java
+++ src/test/java/net/gongple/restapi/events/EventControllerTests.java
@@ -1,16 +1,24 @@
 package net.gongple.restapi.events;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -19,13 +27,39 @@ public class EventControllerTests {
 	@Autowired
 	MockMvc mockMvc;
 	
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	@MockBean
+	EventRepository eventRepository;
+	
 	@Test
 	public void createEvent() throws Exception {
-		mockMvc.perform(post("/api/events")
+		Event event = Event.builder()
+				.name("Spring")
+				.description("Rest Api Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2019, 10, 21, 9, 30))
+				.closeEnrollmentDateTime(LocalDateTime.of(2019, 10, 21, 9, 30))
+				.beginEventDateTime(LocalDateTime.of(2019, 10, 31, 9, 30))
+				.endEventDateTime(LocalDateTime.of(2019, 10, 31, 9, 30))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("강남 D2 스타트업 팩토리")
+				.build();
+		event.setId(10);
+		
+		Mockito.when(eventRepository.save(event)).thenReturn(event);
+		// Mockito? 뭐하는 놈이냐?
+		
+		mockMvc.perform(post("/api/events/")
 					.contentType(MediaType.APPLICATION_JSON_UTF8)
 					.accept(MediaTypes.HAL_JSON)
-					)
-				.andExpect(status().isCreated());
+					.content(objectMapper.writeValueAsString(event)))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("id").exists())
+				;
 	}
 	
 }
