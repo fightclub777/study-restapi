33298c103152a3a4e1f054dff09637a32d22ab55
선영 맹 <imdct@seon-yeong-maeng-ui-MacBook-Pro.local>
2019-10-29 21:16:47 +0900

    RestDocs

--- src/test/java/net/gongple/restapi/events/EventControllerTests.java
+++ src/test/java/net/gongple/restapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package net.gongple.restapi.events;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -8,12 +9,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDateTime;
 
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -22,12 +24,15 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import net.gongple.restapi.common.RestDocsConfiguration;
 import net.gongple.restapi.common.TestDescription;
 
 @RunWith(SpringRunner.class)
 //@WebMvcTest
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 public class EventControllerTests {
 	
 	@Autowired
@@ -73,6 +78,8 @@ public class EventControllerTests {
 					.andExpect(jsonPath("_links.self").exists())
 					.andExpect(jsonPath("_links.query-events").exists())
 					.andExpect(jsonPath("_links.update-event").exists())
+					
+					.andDo(document("create-event"))
 					; 
 	}
 	
